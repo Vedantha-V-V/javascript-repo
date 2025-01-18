8dae2168965b391d9f0d517ca1c0ff1317c09fc5
Vedantha Vinay Vernekar <vernekar.vedantha2004@gmail.com>
2025-01-18 16:53:52 +0000

    Variables and Datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+/*
+Prefer not to use var
+ because of issue in block scope and functional scope
+*/
+
+let accountId=1023
+console.log(accountId)
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";//treat all JS code as newer version
+
+// alert(3+3) //Using nodejs not server
+
+console.log(3+3);
+
+let name = "Vedantha"
+let age = 18
+let isLoggedIn = false
+
+//number => 2^53
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined 
+//symbol => unique
+
+//object
+
+console.log(typeof name);
