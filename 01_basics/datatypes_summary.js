6fe311b6c0ba9b26b22557f3f0e6e2f42a33cd5c
Vedantha Vinay Vernekar <vernekar.vedantha2004@gmail.com>
2025-01-19 17:24:06 +0000

    DataType Summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,25 @@
+// Primitive 
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+
+const id = Symbol('123')
+
+console.log(id)
+
+// Reference (Non Primitive)
+
+//Array, Objects, Functions
+
+const myFunction = function(){
+    console.log("Hello Wordle")
+}
+
+console.log(typeof myFunction)
+
+//https://262.ecma-international.org/5.1/#sec-11.4.3
