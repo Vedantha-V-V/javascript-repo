4cd45e570e204341f89917e13aa766acb54bee3a
Vedantha Vinay Vernekar <vernekar.vedantha2004@gmail.com>
2025-01-19 16:45:36 +0000

    Opertions

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,23 @@
+let value = 3
+let negValue = -value
+console.log(negValue)
+console.log(2**2) //Exponential value
+
+let str1 = "hello"
+let str2 = " vedantha"
+let str3 =str1+str2
+console.log(str3);
+
+console.log(1+3+"2");
+
+console.log(2 > 1);
+
+console.log("2" > 1);
+
+console.log(undefined == 0); //avoided
+console.log(null > 0)//avoided
+
+console.log("2" == 2);
+
+//link to study
+//https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
