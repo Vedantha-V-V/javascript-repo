a8d28a8678548035503e7ccc5a4558599c59c3ad
Vedantha Vinay Vernekar <vernekar.vedantha2004@gmail.com>
2025-01-19 16:24:49 +0000

    Conversion of Data Types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = 33
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" =>NaN
+//true =>1; false => 0
+
+let isLoggedIn = 1
+booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(isLoggedIn)
+
+//1 => true; 0 => false
+//"" => false
+//"hitesh" => true
