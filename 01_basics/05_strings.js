4ccb1d5866ee8d1221b90c040e9ea72f1aee5b83
Vedantha Vinay Vernekar <vernekar.vedantha2004@gmail.com>
2025-01-20 09:35:38 +0000

    String Basics

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,15 @@
+const name = "vedantha"
+repoCount = 7
+
+console.log(`This repo belongs to ${name} with number of repositories is ${repoCount}`);
+
+const gameName = new String("triple-v");
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.length);
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4);
+
+const newStringOne = "    vedantha   "
+console.log(newStringOne.trim());
