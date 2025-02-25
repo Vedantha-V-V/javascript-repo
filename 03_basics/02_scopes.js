8d9e11d03955817af47137e7e2a933e2473a19e9
Vedantha Vinay Vernekar <vernekar.vedantha2004@gmail.com>
2025-02-25 14:18:02 +0000

    Hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -32,3 +32,17 @@ if(true){
 
 //console.log(username) <--- error
 
+
+//++++++++++++++++++++ Interesting +++++++++++++++++++
+
+console.log(addone(5))
+
+function addone(num){
+    return num+1
+}
+
+const addTwo = function(num){
+    return num+2
+}
+
+console.log(addTwo(5))
