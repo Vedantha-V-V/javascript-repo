a2954ff319a1cf2fd90d425dae1e847d82d85a97
Vedantha Vinay Vernekar <vernekar.vedantha2004@gmail.com>
2025-02-25 14:13:30 +0000

    Scopes-2

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -8,3 +8,27 @@ if (true){
 }
 
 console.log("Global Scope: ",a)
+
+function one(){
+    const username = "vedantha"
+    function two(){
+        const website = "youtube"
+        console.log(username)
+    }
+    //console.log(website) <-- Throws an error
+    two()
+}
+
+one()
+
+if(true){
+    const username = "vedantha"
+    if(username === "vedantha"){
+        const website = " youtube"
+        console.log(username + website)
+    }
+    //console.log(website) <--- error
+}
+
+//console.log(username) <--- error
+
